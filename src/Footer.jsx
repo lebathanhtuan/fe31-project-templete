11fd8109a898f91dcedfb99e62ee12742e1da930
Le Ba Thanh Tuan <le.ba.thanh.tuan@sun-asterisk.com>
2023-03-06 14:06:25 +0700

    Render list and Events

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,12 +2,7 @@ export const name = "Tuấn";
 export const className = "FE31";
 
 function Footer() {
-  return (
-    <>
-      <div className="footer">Footer</div>
-      <div>Ahihi</div>
-    </>
-  );
+  return <div className="footer">Footer</div>;
 }
 
 export default Footer;
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,55 @@
 function Main() {
-  return <div className="main">Main</div>;
+  const name = "Tuan";
+  const productList = [
+    {
+      name: "iPhone 14",
+      price: 999,
+    },
+    {
+      name: "iPhone 14 Pro",
+      price: 1999,
+    },
+    {
+      name: "iPhone 14 Pro Max",
+      price: 2999,
+    },
+  ];
+
+  const handleBuyProduct = (e, name) => {
+    console.log(e.target);
+    console.log(`buy ${name}`);
+  };
+
+  const notiAhihi = () => {
+    console.log("hihi");
+  };
+
+  const handleChangeSearchKey = (e) => {
+    console.log(e.target.value);
+  };
+
+  const renderProductList = () => {
+    return productList.map((item, index) => {
+      return (
+        <div key={index} className="product-item">
+          <h2>{item.name}</h2>
+          <h3>{item.price}</h3>
+          <button onClick={(e) => handleBuyProduct(e, item.name)}>Buy</button>
+          <button onClick={() => notiAhihi()}>Ahihi</button>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div className="main">
+      <div>{name}</div>
+      <div>
+        <input name="searchKey" onChange={(e) => handleChangeSearchKey(e)} />
+      </div>
+      <div>{renderProductList()}</div>
+    </div>
+  );
 }
 
 export default Main;
